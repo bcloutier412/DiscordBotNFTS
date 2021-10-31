f6e77a65c1214b8dc1175c90aed7727bc042487a
bcloutier412 <85542673+bcloutier412@users.noreply.github.com>
2021-10-31 04:49:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+import random
+import time
+
+# token = "MjAzMjM3NTAxODMyMjY1NzMw.YX0Ryg.Qay0pJ5whr5LhDA-s-pXKZV9Xi0"
+# channel_id = 903943388527742978
+
+token = input("What is your token? :")
+channel_id = int(input("What is the channel_id? :"))
+delay = int(input("How long is the delay in seconds? :"))
+message_count = int(input("How many messages do you want to send? :"))
+
+
+def send_message(token, channel_id, message):
+    url = 'https://discord.com/api/v9/channels/{}/messages'.format(channel_id)
+    data = {"content": message}
+    header = {"authorization": token}
+
+    r = requests.post(url, data=data, headers=header)
+    print(r.status_code)
+
+
+messages = ['Aye whats up guys hows it going today?', 'LOLOL', 'XDXD', 'nathan is an idiot']
+
+for i in range(message_count):
+    send_message(token, channel_id, random.choice(messages))
+    time.sleep(delay)
