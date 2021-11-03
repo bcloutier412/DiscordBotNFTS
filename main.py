122ca361ba264e1c5bd5e7c36e580c048e2ec073
bcloutier412 <85542673+bcloutier412@users.noreply.github.com>
2021-11-03 04:56:04 +0000

    Rewrote the whole code to be more user friendly and allow multi account functionality

--- main.py
+++ main.py
@@ -1,26 +1,51 @@
 import requests
 import random
 import time
+'''
+1. Gather User Information
+    channel_list
+    create list of User() objects
+        tokens
+    delay
+    message count
+2. Using a for loop, iterate through the object list and use object.send_messages 
+to random message to all channels in the channel list
+'''
 
-# channel_id = 903943388527742978
+message_list = ['hello', 'herro']
+class User():
+    def __init__(self, token):
+        self.token = token
 
-token = input("What is your token? :")
-channel_id = int(input("What is the channel_id? :"))
-delay = int(input("How long is the delay in seconds? :"))
-message_count = int(input("How many messages do you want to send? :"))
+    def send_message(self, channel_id, message):
+        url = 'https://discord.com/api/v9/channels/{}/messages'.format(channel_id)
+        data = {"content": message}
+        header = {"authorization": self.token}
 
+        r = requests.post(url, data=data, headers=header)
+        print(r.status_code)
 
-def send_message(token, channel_id, message):
-    url = 'https://discord.com/api/v9/channels/{}/messages'.format(channel_id)
-    data = {"content": message}
-    header = {"authorization": token}
+    def send_messages(self):
+        for channel in channel_list:
+            self.send_message(channel, random.choice(message_list))
 
-    r = requests.post(url, data=data, headers=header)
-    print(r.status_code)
+channel_list = []
+object_list = []
 
+for i in range(int(input("How many channels do you want to send to? :"))):
+    channel_list.append(int(input("Input Channel_{}_id :".format(i + 1))))
 
-messages = ['Aye whats up guys hows it going today?', 'LOLOL', 'XDXD', 'nathan is an idiot']
+for i in range(1, int(input("How many discord accounts do you have? :")) + 1):
+    token = input("What is the discords token? :")
+    object_list.append(User(token))
 
-for i in range(message_count):
-    send_message(token, channel_id, random.choice(messages))
+delay = int(input("What is the higher channel cooldown? : "))
+message_count = int(input("How many messages do you want to send? : "))
+
+# for object in object_list:
+#     for message in range(message_count):
+#         object.send_messages()
+for message in range(message_count):
+    for object in object_list:
+        object.send_messages()
     time.sleep(delay)
